56975d84dd453d6dbc3514c347b3c522f73f29e3
Libing Chen <libing.chen@gmail.com>
2023-01-12 10:45:06 +0800

    chore: adjust bean injection

--- src/main/java/org/mvnsearch/spring/boot/trpc/flux/TrpcWebFluxConfigurer.java
+++ src/main/java/org/mvnsearch/spring/boot/trpc/flux/TrpcWebFluxConfigurer.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.ReactiveAdapterRegistry;
 import org.springframework.http.codec.HttpMessageReader;
+import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
 
@@ -17,13 +19,18 @@ public class TrpcWebFluxConfigurer implements WebFluxConfigurer {
     @Autowired
     private ObjectMapper objectMapper;
     @Autowired
-    List<HttpMessageReader<?>> readers;
-    @Autowired
-    ReactiveAdapterRegistry registry;
+    @Lazy
+    private ReactiveAdapterRegistry registry;
+    private List<HttpMessageReader<?>> readers;
 
     @Override
     public void configureArgumentResolvers(ArgumentResolverConfigurer configurer) {
         configurer.addCustomResolver(new TrpcInputArgumentResolver(this.readers, this.registry, this.objectMapper));
+    }
+
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        this.readers = configurer.getReaders();
     }
 
 }
